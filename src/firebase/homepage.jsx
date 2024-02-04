26c21b77c3506348f9813e05d0ac431f1cecbce2
Ola Olasunkanmi <finixd531@gmail.com>
2024-02-04 21:10:21 +0100

    vacel fix 2

--- src/firebase/homepage.jsx
+++ src/firebase/homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from '@mui/material'
 import { Route,Routes } from 'react-router-dom'
 import SignUp from './SignUp';
-import LogIn from './Login';
+import LogIn from './LogIn';
 
 
 function homepage() {
